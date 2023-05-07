a5867ce799d781d7a865f553199d12c1d6684065
Yaroslav-Hryshenko <yaroslav.cook81@gmail.com>
2023-05-07 09:23:46 +0300

    hw8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryList = document.querySelector('.gallery');
 
+// Add gallery items to list
+galleryItems.forEach(({ preview, original, description }) => {
+  const galleryItem = document.createElement('li');
+  galleryItem.classList.add('gallery__item');
+
+  const link = document.createElement('a');
+  link.classList.add('gallery__link');
+  link.href = original;
+
+  const image = document.createElement('img');
+  image.classList.add('gallery__image');
+  image.src = preview;
+  image.alt = description;
+
+  link.append(image);
+  galleryItem.append(link);
+  galleryList.append(galleryItem);
+});
+
+// Inicialize SimpleLightbox
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+console.log(galleryItems);
 console.log(galleryItems);
