b8bc524f084e7d320a063ea39bfec5b36619a387
Yaroslav-Hryshenko <yaroslav.cook81@gmail.com>
2023-05-08 08:26:25 +0300

    hw8/2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const PLAY_VIDEO = 'vidioplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function ({ seconds }) {
+  localStorage.setItem(PLAY_VIDEO, seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const currentTime = Number(localStorage.getItem(PLAY_VIDEO));
+
+ if (currentTime === null) {
+   return;
+ } else {
+   console.log(JSON.parse(currentTime));
+   player.setCurrentTime(currentTime);
+ }
